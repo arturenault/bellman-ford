c210cb77b43e59166fdee1e59002ee7e1f6d01a1
Artur Renault <artur.renault@gmail.com>
2014-12-10 12:57:46 -0500

    linkup and linkdown tentatively working

--- bfclient.py
+++ bfclient.py
@@ -9,45 +9,54 @@ import socket
 import sys
 
 
+# Function to prompt the user for a new command
 def prompt():
     sys.stdout.write("> ")
     sys.stdout.flush()
 
 
+# Function to exit gracefully
 def quit(signum, frame):
-    print
+    print  # New line after ^C
     exit(0)
 
 
+# Advertises routing table to other nodes
 def advertise(signum, frame):
     advertisement = datagram.pack(here, "UPDATE", routes)
     for link in neighbors:
-        neighbors[link].send(advertisement)
-    signal.alarm(timeout)
+        if neighbors[link].distance < float("inf"):
+            neighbors[link].send(advertisement)
+    signal.alarm(timeout) # timeout handled by alarm.
+                          # This way user input won't reset the timeout.
     signal.signal(signal.SIGALRM, advertise)
 
 
+# Main method
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, quit)
 
+    # Handle command line args
     if len(sys.argv) < 6 or len(sys.argv) % 3 != 0:
         exit("usage: ./bfclient.py localport timeout [ipaddress1 port1 weight1 ...]")
-
     local_addr = socket.gethostbyname(socket.gethostname())
     local_port = int(sys.argv[1])
     here = local_addr + ":" + str(local_port)
     timeout = int(sys.argv[2])
 
+    # Prepare receiving socket
     in_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     in_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     in_sock.bind(("", local_port))
 
-    neighbors = dict()
-    original_links = dict()
-    routes = dict()
-    last_seen = dict()
+    # Prepare data structures
+    neighbors = dict()  # Neighboring links
+    original_links = dict()  # Store original distances for LINKUP
+    routes = dict()  # Routing table
+    last_seen = dict()  # To keep track of recent activity
 
     for i in range(3, len(sys.argv), 3):
+        # Resolves inconsistencies across routing tables
         if sys.argv[i] == "localhost" or sys.argv[i] == "127.0.0.1":
             new_host = socket.gethostname()
         else:
@@ -65,63 +74,87 @@ if __name__ == "__main__":
         routes[new_route.id] = new_route
         last_seen[new_route.id] = datetime.datetime.now()
 
-
     advertise(0, 0)
-
     prompt()
+
     while True:
         try:
             ready, spam, eggs = select.select([in_sock, sys.stdin], [], [], timeout)
             if ready:
                 for source in ready:
+
+                    # Handle new messages
                     if source is in_sock:
+
+                        # I decided not to use the address returned by recvfrom.
+                        # I feel like that's cheating, in the network layer
                         received, ham = source.recvfrom(4096)
-                        recv_source, recv_time, command, data = datagram.unpack(received)
-                        if recv_source not in last_seen or recv_time > last_seen[recv_source]:
-                            last_seen[recv_source] = datetime.datetime.now()
-                            if command == "UPDATE":
-                                neighbor_table = datagram.dictionary(data)
-                                for destination in neighbor_table:
-                                    if destination == here:
-                                        if recv_source not in neighbors:
-                                            new_link = Link(recv_source, neighbor_table[destination])
-                                            neighbors[recv_source] = new_link
-                                            original_links[recv_source] = new_link.distance
+                        messages = filter(bool, received.split("END\n"))
+                        for message in messages:
+                            recv_source, recv_time, command, data = datagram.unpack(message)
 
-                                        if recv_source not in routes:
-                                            new_route = Route(recv_source, neighbor_table[destination],
-                                                              neighbors[recv_source])
-                                            routes[recv_source] = new_route
+                            # Disregard out-of-order packets
+                            if recv_source not in last_seen or last_seen[recv_source] < recv_time:
+                                last_seen[recv_source] = datetime.datetime.now()
 
-                                        elif neighbor_table[destination] != routes[recv_source].distance:
-                                            routes[recv_source].distance = neighbor_table[destination]
-                                            routes[recv_source].link = neighbors[recv_source]
+                                # ROUTE UPDATE
+                                if command == "UPDATE":
+                                    neighbor_table = datagram.dictionary(data)
+                                    for destination in neighbor_table:
 
-                                    else:
-                                        if destination not in routes:
-                                            routes[destination] = Route(destination,
-                                                                        neighbor_table[destination] + neighbors[
-                                                                            recv_source].distance,
-                                                                        neighbors[recv_source])
-                                            advertise(0, 0)
-                                        elif routes[destination].link is neighbors[recv_source] and routes[
-                                            destination].distance != neighbor_table[destination] \
-                                                or routes[destination] > neighbor_table[destination] + neighbors[
-                                                    recv_source].distance:
-                                            routes[destination].distance = neighbor_table[destination] + neighbors[
-                                                recv_source].distance
-                                            routes[destination].link = neighbors[recv_source]
-                                            advertise(0, 0)
-                            elif command == "LINKDOWN":
-                                neighbors[recv_source].distance = sys.maxint
-                                for destination in routes:
-                                    if routes[destination].link.id == down_id:
-                                        routes[destination].distance = sys.maxint
-                                advertise(0,0)
-                            elif command =="LINKUP":
-                                neighbors[recv_source].distance = original_links[recv_source].distance
-                                routes[recv_source].distance = original_links[recv_source].distance
+                                        # Refers to current link
+                                        if destination == here:
 
+                                            # New link
+                                            if recv_source not in neighbors:
+                                                new_link = Link(recv_source, neighbor_table[destination])
+                                                neighbors[recv_source] = new_link
+                                                original_links[recv_source] = new_link.distance
+
+                                                if recv_source not in routes:
+                                                    new_route = Route(recv_source, neighbor_table[destination],
+                                                                  neighbors[recv_source])
+                                                    routes[recv_source] = new_route
+
+                                            # Link change (always assume received version is right)
+                                            if neighbor_table[destination] != neighbors[recv_source].distance:
+                                                diff = neighbor_table[destination] - neighbors[recv_source].distance
+                                                neighbors[recv_source].distance += diff
+                                                for host in routes:
+                                                    if routes[host].link.id == recv_source:
+                                                        routes[recv_source].distance += neighbor_table[destination]
+
+                                        else:
+                                            total_distance = neighbors[recv_source].distance + neighbor_table[destination]
+
+                                            # New route
+                                            if destination not in routes:
+                                                new_route = Route(destination,
+                                                                  neighbor_table[destination] + neighbors[
+                                                                      recv_source].distance,
+                                                                  neighbors[recv_source])
+                                                routes[destination] = new_route
+                                                advertise(0, 0)
+
+                                            # Better route, or change in current one
+                                            elif routes[destination].link.id == recv_source and routes[destination].distance != total_distance \
+                                                    or routes[destination].distance > total_distance:
+                                                routes[destination].distance = total_distance
+                                                routes[destination].link = neighbors[recv_source]
+                                                advertise(0, 0)
+
+                                elif command == "LINKDOWN":
+                                    neighbors[recv_source].distance = float("inf")
+                                    for destination in routes:
+                                        if routes[destination].link.id == recv_source:
+                                            routes[destination].distance = float("inf")
+                                    advertise(0, 0)
+                                elif command == "LINKUP":
+                                    neighbors[recv_source].distance = original_links[recv_source]
+                                    routes[recv_source].distance = original_links[recv_source]
+                                    for i in routes:
+                                        print i + " -> " + str(routes[i].distance)
+                                    advertise(0, 0)
 
                     else:
                         args = sys.stdin.readline().strip().split(" ")
@@ -136,13 +169,14 @@ if __name__ == "__main__":
                                 else:
                                     down_addr = args[1]
                                 down_id = socket.gethostbyname(down_addr) + ":" + args[2]
-                                down_message = datagram.pack(down_id, "LINKDOWN")
-                                neighbors[down_id].distance = sys.maxint
+                                down_message = datagram.pack(here, "LINKDOWN")
+                                neighbors[down_id].send(down_message)
+                                neighbors[down_id].distance = float("inf")
                                 last_seen[down_id] = datetime.datetime.now()
                                 for destination in routes:
                                     if routes[destination].link.id == down_id:
-                                        routes[destination].distance = sys.maxint
-                                advertise(0,0)
+                                        routes[destination].distance = float("inf")
+                                advertise(0, 0)
 
                             except IndexError:
                                 print("ERROR: command format incorrect.")
@@ -150,8 +184,18 @@ if __name__ == "__main__":
                                 print("ERROR: link does not exist.")
                         elif command == "LINKUP":
                             try:
-                                up_id = args[1] + ":" + args[2]
+                                if args[1] == "localhost" or args[1] == "127.0.0.1":
+                                    up_addr = socket.gethostname()
+                                else:
+                                    up_addr = args[1]
+                                up_id = socket.gethostbyname(up_addr) + ":" + args[2]
+                                up_message = datagram.pack(here, "LINKUP")
+                                neighbors[up_id].send(up_message)
                                 neighbors[up_id].distance = original_links[up_id]
+                                routes[up_id].distance = original_links[up_id]
+                                last_seen[up_id] = datetime.datetime.now()
+                                advertise(0, 0)
+
                             except IndexError:
                                 print("ERROR: command format incorrect.")
                             except KeyError:
@@ -163,6 +207,7 @@ if __name__ == "__main__":
                         prompt()
 
         except select.error:
+            # If timeout happens while select is waiting
             advertise(0, 0)
 
         for host in last_seen:
